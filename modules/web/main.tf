a8c6fdda5aafdf94c3106d965ec63d181c8ac3e7
Sarah <saraldowihi@gmail.com>
2024-09-07 20:59:21 +0300

    fix-typo

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -55,6 +55,6 @@ resource "google_compute_instance" "web" {
       # Leave empty for dynamic public IP
     }
   }  
- metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/wwww/html/index.html"
+ metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
  allow_stopping_for_update = true 
 }
