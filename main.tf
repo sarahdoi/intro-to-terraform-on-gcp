85049e148b84d57751fc86fcb852895ebb08cc3e
Sarah <saraldowihi@gmail.com>
2024-09-07 20:24:53 +0300

    add staging and prod

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ module "qa" {
     network_name = "qa"
 
 }
+
+module "staging" {
+    source= "./modules/web" 
+    project_id = var.project_id
+    region = var.region 
+
+    app_name = "staging-web"
+    network_name = "staging"
+
+}
+
+module "prod" {
+    source= "./modules/web" 
+    project_id = var.project_id
+    region = var.region 
+
+    app_name = "prod-web"
+    network_name = "prod"
+
+}
